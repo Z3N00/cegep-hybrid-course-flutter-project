64cbb525be7e94d116e5033a03ef84fc7c70dc9e
Kartik Gupta <kartikgupta676@gmail.com>
2021-11-30 10:51:00 -0500

    adding assets and libraries

--- lib/widgets/custom_bottom_navigation_bar.dart
+++ lib/widgets/custom_bottom_navigation_bar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class CustomBottomNavigationBar extends StatelessWidget {
   const CustomBottomNavigationBar({Key? key, required this.selectedPageIndex, required this.onIconTap}) : super(key: key);
@@ -6,8 +7,31 @@ class CustomBottomNavigationBar extends StatelessWidget {
   final Function onIconTap;
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [],
+    final barHeight = MediaQuery.of(context).size.height * 0.06;
+    return BottomAppBar(
+      child: Container(
+        height: barHeight,
+        child: Row(
+          children: [_bottombarNavitem(0, 'Home', TextStyle(), 'home')],
+        ),
+      ),
     );
   }
+
+  _bottombarNavitem(int index, String label, TextStyle textStyle, String iconName){
+
+    bool isSelected = selectedPageIndex == index;
+    Color iconAndTextColor = isSelected ? Colors.black : Colors.grey;
+
+    if(isSelected && selectedPageIndex == 0){
+      iconAndTextColor = Colors.grey;
+    }
+
+    return Column(children: [
+      SvgPicture.asset('assets/${isSelected ? iconName + '_filled' : iconName}.svg'),
+      const SizedBox(height: 3,),
+      Text(label, style: textStyle.copyWith(color: iconAndTextColor),)
+    ],);
+  }
+
 }
