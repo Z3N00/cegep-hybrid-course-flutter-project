a36b2fb4f9c2f087c2d1a8e94427ca5fd2490306
Kartik Gupta <kartikgupta676@gmail.com>
2021-12-05 10:24:12 -0500

    updating home side bar

--- lib/widgets/home_side_bar.dart
+++ lib/widgets/home_side_bar.dart
@@ -1,17 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomeSideBar extends StatelessWidget {
   const HomeSideBar({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        _profileImageButton()
-      ],
+    TextStyle style =   Theme.of(context).textTheme.bodyText1!.copyWith(fontSize: 13, color: Colors.white);
+    return Padding(
+      padding: EdgeInsets.only(right: 8.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          _profileImageButton(),
+          _sideBarItem('heart', '1.2M', style),
+          _sideBarItem("comment", "1.2k", style),
+          _sideBarItem("share", "Share", style)
+        ],
+      ),
     );
   }
 
+  _sideBarItem(String iconName, String label, TextStyle style){
+    return Column(
+      children: [
+
+        SvgPicture.asset('assets/$iconName.svg'),
+        SizedBox(height: 5.0,),
+        Text(label, style: style,)
+      ],
+    );
+  }
   _profileImageButton(){
     return Stack(
       clipBehavior: Clip.none,
@@ -35,7 +55,7 @@ class HomeSideBar extends StatelessWidget {
                 color: Colors.red,
                 borderRadius: BorderRadius.circular(25)
               ),
-              child: Icon(Icons.add, size: 20,),
+              child: Icon(Icons.add, size: 20, color: Colors.white,),
             ))
       ],
     );
