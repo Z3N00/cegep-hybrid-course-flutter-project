72df0f772c8db6c9cbe5bee0e2e05f1974133e34
Inderpreet Singh <94181947+inder431@users.noreply.github.com>
2021-12-09 11:08:22 -0500

    Update profile_page.dart

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -6,8 +6,37 @@ class ProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blue,
+    return Scaffold(
+
+
+
+    appBar: AppBar(
+    title: Text('Profile',textAlign: TextAlign.center,style: TextStyle(color: Colors.black)),
+      backgroundColor: Colors.white,
+    ),
+      body: Center(child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+        Icon(Icons.person_outline,
+          color: Colors.grey,
+          size:100.0),
+          SizedBox(height:15.0,),
+          Text('Sign Up for an account'),
+          SizedBox(width:200.0,
+              child: Builder(
+            builder: (context) => RaisedButton(onPressed: () =>{},
+            color: Colors.red[400],
+              textColor:Colors.white,
+              child: Text('Sign up')
+
+            ),
+      ),
+    ),
+        ]
+      ),
+      ),
     );
+
+
   }
 }
