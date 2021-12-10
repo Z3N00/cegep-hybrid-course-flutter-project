9a7d8009b6e8e50ecbe2e092624afb4f35601f06
Kartik Gupta <kartikgupta676@gmail.com>
2021-12-10 18:49:32 -0500

    adding discover page

--- lib/pages/discover_page.dart
+++ lib/pages/discover_page.dart
@@ -1,13 +1,178 @@
+import 'dart:async';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:page_view_indicators/circle_page_indicator.dart';
+// page_view_indicators: ^2.0.0
 
+final List<String> imgList = [
+  'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+  'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
+  'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
+  'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
+  'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
+  'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
+];
 
-class DiscoverPage extends StatelessWidget {
+class DiscoverPage extends StatefulWidget {
   const DiscoverPage({Key? key}) : super(key: key);
 
   @override
+  State<DiscoverPage> createState() => _DiscoverPageState();
+}
+
+class _DiscoverPageState extends State<DiscoverPage> {
+  final PageController _pageController = PageController();
+  final _currentPageNotifier = ValueNotifier<int>(0);
+
+  int _currentPage = 0;
+  late Timer _timer;
+
+  // @override
+  // void initState() {
+  //   _timer = Timer.periodic(const Duration(seconds: 5), (timer) {
+  //     if(_currentPage<5) {
+  //       _currentPage++;
+  //     } else {
+  //       _currentPage = 0;
+  //     }
+  //     _pageController.animateToPage(_currentPage,
+  //         duration: const Duration(microseconds: 400), curve: Curves.ease);
+  //   });
+  //   super.initState();
+  // }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _pageController.dispose();
+    _timer.cancel();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.amber,
+    return CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          backgroundColor: Colors.white,
+          elevation: 0.25,
+          pinned: true,
+          centerTitle: false,
+          title: Container(
+            height: 35,
+            decoration: BoxDecoration(
+                color: Colors.grey.shade200,
+                borderRadius: BorderRadius.circular(2)
+            ),
+            child: TextField(
+              textAlignVertical: TextAlignVertical.center,
+              cursorColor: Colors.grey.shade500,
+              decoration: InputDecoration(
+                hintText: "Search",
+                hintStyle: Theme.of(context).textTheme.bodyText1!.copyWith(color: Colors.grey.shade500,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500),
+                border: InputBorder.none,
+                prefixIcon: const Icon(CupertinoIcons.search,
+                    color: Colors.black),
+              ),
+              style: Theme.of(context).textTheme.bodyText1!.copyWith(
+                  color: Colors.black,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500
+              ),
+            ),
+          ),
+          actions: [
+            IconButton(
+                onPressed: () {},
+                icon: const Icon(CupertinoIcons.qrcode_viewfinder,
+                  color: Colors.black,))
+          ],
+        ),
+        SliverToBoxAdapter(
+          child: Container(
+            height: MediaQuery.of(context).size.height / 4,
+            child: Stack(
+              alignment: Alignment.bottomCenter,
+              children: [
+                PageView.builder(
+
+                    controller: _pageController,
+                    onPageChanged: (int page) => {
+                      _currentPageNotifier.value = page
+                    },
+                    itemCount: 5,
+                    itemBuilder: (context, index){
+                      return Container(
+                        child: CarouselSlider(
+                          options: CarouselOptions(
+                            autoPlay: true,
+                            aspectRatio: 2.0,
+                            enlargeCenterPage: true,
+                          ),
+                          items: imageSliders,
+                        ),
+                      );
+
+                    }
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 15),
+                  child: CirclePageIndicator(
+                    currentPageNotifier: _currentPageNotifier,
+                    itemCount: 5,
+                    selectedSize: 5,
+                  ),
+                )
+              ],
+            ),
+          ),
+        )
+      ],
     );
   }
 }
+
+
+final List<Widget> imageSliders = imgList
+    .map((item) => Container(
+  child: Container(
+    margin: EdgeInsets.all(5.0),
+    child: ClipRRect(
+        borderRadius: BorderRadius.all(Radius.circular(5.0)),
+        child: Stack(
+          children: <Widget>[
+            Image.network(item, fit: BoxFit.cover, width: 1000.0),
+            Positioned(
+              bottom: 0.0,
+              left: 0.0,
+              right: 0.0,
+              child: Container(
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      Color.fromARGB(200, 0, 0, 0),
+                      Color.fromARGB(0, 0, 0, 0)
+                    ],
+                    begin: Alignment.bottomCenter,
+                    end: Alignment.topCenter,
+                  ),
+                ),
+                padding: const EdgeInsets.symmetric(
+                    vertical: 10.0, horizontal: 20.0),
+                child: Text(
+                  'No. ${imgList.indexOf(item)} image',
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        )),
+  ),
+)).toList();
