9796bae6894d8141c6513b07c3afd821b1b5b6bc
jaswanthveerepally <75801602+jaswanthveerepally@users.noreply.github.com>
2021-12-12 21:03:43 -0500

    added template selector

--- lib/pages/add_videos_page.dart
+++ lib/pages/add_videos_page.dart
@@ -1,13 +1,88 @@
 import 'package:flutter/material.dart';
 
 
-class AddVideoPage extends StatelessWidget {
+class AddVideoPage extends StatefulWidget {
   const AddVideoPage({Key? key}) : super(key: key);
 
   @override
+  State<AddVideoPage> createState() => _AddVideoPageState();
+}
+
+class _AddVideoPageState extends State<AddVideoPage> {
+
+  PageController _pageController = PageController(initialPage: 1, viewportFraction: 0.2);
+  int _selectedTab = 1;
+
+  @override
+  void dispose() {
+    super.dispose();
+    _pageController.dispose();
+  }
+
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.black,
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [
+        Container(
+          color: Colors.amber,
+        ),
+          Spacer(),
+          Container(
+            color: Colors.black,
+            height: 90,
+            child: _buildCameraTemplateSelector(),
+          )
+      ],),
     );
+  }
+
+  Widget _buildCameraTemplateSelector() {
+    final List<String> postTypes = ["Camera","Quick","Templates"];
+    TextStyle style = Theme.of(context).textTheme.bodyText1!.copyWith(
+        fontSize: 15, color: Colors.white, fontWeight: FontWeight.bold);
+    return Stack(
+      alignment: Alignment.topCenter,
+      children: [
+        Container(height: 45,
+          child: PageView.builder(
+              controller: _pageController ,
+              onPageChanged: (int page) =>{
+                setState(() {
+                  _selectedTab = page;
+                })
+              },
+              itemCount: postTypes.length,
+              itemBuilder: (context, index){
+               return Container(
+                alignment: Alignment.center,
+                width: 50,
+                height: 50,
+                child: Text(
+                postTypes[index],
+                style: style.copyWith(
+                    color:
+                      _selectedTab == index ? Colors.white : Colors.grey ),
+             ),
+           );
+
+          },
+          ),
+        ),
+        Container(
+          width: 50,
+          height: 45,
+          alignment: Alignment.bottomCenter,
+          child: CircleAvatar(
+            backgroundColor: Colors.white,
+            radius: 2.5,
+          ),
+        )
+      ],
+
+    );
+
   }
 }
