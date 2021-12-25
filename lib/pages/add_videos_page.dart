d1d8551220c505b4b3c816b923582c0820a6958f
Kartik Gupta <kartikgupta676@gmail.com>
2021-12-24 23:28:07 -0500

    updating add video features

--- lib/pages/add_videos_page.dart
+++ lib/pages/add_videos_page.dart
@@ -1,8 +1,26 @@
 import 'package:flutter/material.dart';
 
 
-class AddVideoPage extends StatelessWidget {
+class AddVideoPage extends StatefulWidget {
   const AddVideoPage({Key? key}) : super(key: key);
+
+  @override
+  State<AddVideoPage> createState() => _AddVideoPageState();
+}
+
+class _AddVideoPageState extends State<AddVideoPage> {
+
+  PageController _pageController = PageController(
+    initialPage: 1,
+    viewportFraction: 0.2
+  );
+  int _selectedTab = 1;
+
+  @override
+  void dispose() {
+    super.dispose();
+    _pageController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,22 +46,46 @@ class AddVideoPage extends StatelessWidget {
 
  Widget _buildCameraTemplateSelector() {
     final List<String> postTypes = ["Camera", "Quick", "Templates"];
+    TextStyle style = Theme.of(context).textTheme.bodyText1!.copyWith(
+        fontSize: 13,
+        color: Colors.white,
+        fontWeight: FontWeight.bold,
+    );
+
     return Stack(
         alignment: Alignment.topCenter,
         children: [
           Container(
             height: 45,
             child: PageView.builder(
+              controller: _pageController,
+              onPageChanged: (int page) => {
+                setState(() {
+                  _selectedTab = page;
+                })
+              },
               itemCount: postTypes.length,
               itemBuilder: (context, index){
               return Container(
+                alignment: Alignment.center,
                 width: 50,
                 height: 50,
                 child: Text(
-                  postTypes[index]
+                  postTypes[index],
+                  style: style.copyWith(color: _selectedTab == index ? Colors.white : Colors.grey),
                 ),
               );
             }),
+          ),
+          Container(
+            width: 50,
+            height: 45,
+            alignment: Alignment.bottomCenter,
+            child: CircleAvatar(
+              backgroundColor: Colors.white,
+              radius: 2.5,
+
+            ),
           )
         ],
       );
