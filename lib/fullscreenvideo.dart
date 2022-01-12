4d820a29b04a720bc1a20e9f64d8ae0766db6113
jaswanthveerepally <75801602+jaswanthveerepally@users.noreply.github.com>
2022-01-11 20:24:06 -0500

    adding more videos

--- lib/fullscreenvideo.dart
+++ lib/fullscreenvideo.dart
@@ -134,4 +134,142 @@
 //     super.dispose();
 //   }
 // }
+// import 'package:flutter/material.dart';
+// import 'package:video_player/video_player.dart';
+//
+// adding comments
+// class FullScreenVideo extends StatefulWidget {
+//   final String videoUrl;   // Video Url from our asset folder
+//   const FullScreenVideo({required this.videoUrl});
+//
+//   @override
+//   State<FullScreenVideo> createState() => _FullScreenVideoState();
+// }
+//
+// class _FullScreenVideoState extends State<FullScreenVideo> {
+//
+//   late VideoPlayerController videoPlayerController;  // Controller to access the videos
+//
+//
+//   @override
+//   void initState() {
+//     // TODO: implement initState
+//     // initial state of our app and initialising
+//     videoPlayerController = VideoPlayerController.asset(widget.videoUrl)..initialize().then((_){
+//       setState(() {
+//         videoPlayerController.play();
+//         videoPlayerController.setLooping(true);
+//       });
+//
+//     });
+//
+//     super.initState();
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return VideoPlayer(videoPlayerController);
+//   }
+//
+//   @override
+//   void dispose() {
+//     // TODO: implement dispose
+//     // disposing videos to avoid memory leaks
+//     videoPlayerController.dispose();
+//     super.dispose();
+//   }
+// }
+
+// import 'package:flutter/material.dart';
+// import 'package:video_player/video_player.dart';
+//
+// adding comments
+// class FullScreenVideo extends StatefulWidget {
+//   final String videoUrl;   // Video Url from our asset folder
+//   const FullScreenVideo({required this.videoUrl});
+//
+//   @override
+//   State<FullScreenVideo> createState() => _FullScreenVideoState();
+// }
+//
+// class _FullScreenVideoState extends State<FullScreenVideo> {
+//
+//   late VideoPlayerController videoPlayerController;  // Controller to access the videos
+//
+//
+//   @override
+//   void initState() {
+//     // TODO: implement initState
+//     // initial state of our app and initialising
+//     videoPlayerController = VideoPlayerController.asset(widget.videoUrl)..initialize().then((_){
+//       setState(() {
+//         videoPlayerController.play();
+//         videoPlayerController.setLooping(true);
+//       });
+//
+//     });
+//
+//     super.initState();
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return VideoPlayer(videoPlayerController);
+//   }
+//
+//   @override
+//   void dispose() {
+//     // TODO: implement dispose
+//     // disposing videos to avoid memory leaks
+//     videoPlayerController.dispose();
+//     super.dispose();
+//   }
+// }
+// import 'package:flutter/material.dart';
+// import 'package:video_player/video_player.dart';
+//
+// adding comments
+// class FullScreenVideo extends StatefulWidget {
+//   final String videoUrl;   // Video Url from our asset folder
+//   const FullScreenVideo({required this.videoUrl});
+//
+//   @override
+//   State<FullScreenVideo> createState() => _FullScreenVideoState();
+// }
+//
+// class _FullScreenVideoState extends State<FullScreenVideo> {
+//
+//   late VideoPlayerController videoPlayerController;  // Controller to access the videos
+//
+//
+//   @override
+//   void initState() {
+//     // TODO: implement initState
+//     // initial state of our app and initialising
+//     videoPlayerController = VideoPlayerController.asset(widget.videoUrl)..initialize().then((_){
+//       setState(() {
+//         videoPlayerController.play();
+//         videoPlayerController.setLooping(true);
+//       });
+//
+//     });
+//
+//     super.initState();
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return VideoPlayer(videoPlayerController);
+//   }
+//
+//   @override
+//   void dispose() {
+//     // TODO: implement dispose
+//     // disposing videos to avoid memory leaks
+//     videoPlayerController.dispose();
+//     super.dispose();
+//   }
+// }
+
+
 
